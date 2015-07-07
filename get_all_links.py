08a5614dc58539e82b6b2140f305158c3a828a1f
André Rainho <arainho.it@gmail.com>
2015-07-07 15:12:24 +0100

    get all links from a website

---
+++ get_all_links.py
@@ -0,0 +1,17 @@
+__author__ = 'raimaz'
+
+import urllib2
+import re
+
+url="www.google.com"
+
+#connect to a URL
+website = urllib2.urlopen(url)
+
+#read html code
+html = website.read()
+
+#use re.findall to get all the links
+links = re.findall('"((http|ftp)s?://.*?)"', html)
+
+print links
