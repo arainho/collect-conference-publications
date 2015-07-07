7f56041f9b56493fa97913042d0a050fa70c75cf
André Rainho <arainho.it@gmail.com>
2015-07-07 16:14:04 +0100

    testing grab href and contens

--- get_publications_pdf.py
+++ get_publications_pdf.py
@@ -6,10 +6,17 @@ import wget
 
 #my_url= input("Enter a website to extract publications from")
 my_url="http://alcatel-lucent.cvent.com/Surveys/Questions/SurveyMain.aspx?r=8a6cedab-dbe0-442d-a76f-c4ef13035c14&ma=1"
-my_website = urllib2.urlopen(my_url)
-my_html = my_website.read()
+my_content = urllib2.urlopen(my_url).read()
 
-my_soup = BeautifulSoup(my_html, "lxml")
+my_soup = BeautifulSoup(my_content, "lxml")
+#print my_soup.prettify()
 
-print(my_soup)
+for my_item in my_soup:
+    tags = my_soup.findAll('a')
+    tags_content= my_soup.a
+    file_name = tags_content.contents[0]+".pdf"
+    my_links = tags["href"]
+    print(my_links, file_name)
+    #wget.download(my_links, file_name)
+pass
 
