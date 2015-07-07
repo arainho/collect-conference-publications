463fd0ed38705e8e546876c1e76df29ffc6e9d7c
André Rainho <arainho.it@gmail.com>
2015-07-07 17:54:03 +0100

    - find a tag - get href items - replace spaces - replace forward slash - download pdf files

--- get_publications_pdf.py
+++ get_publications_pdf.py
@@ -1,23 +1,38 @@
 #encoding=utf-8
-
-__author__ = 'raimaz'
+__author__ = 'Andre Rainho '
+__email__ = 'arainho.it@gmail.com'
 
 from bs4 import BeautifulSoup
 import urllib2
 import wget
 
-#my_url= input("Enter a website to extract publications from")
-my_url="http://alcatel-lucent.cvent.com/Surveys/Questions/SurveyMain.aspx?r=8a6cedab-dbe0-442d-a76f-c4ef13035c14&ma=1"
+my_url = raw_input("Enter a website to extract pdf publications from: ")
 my_content = urllib2.urlopen(my_url).read()
 
-my_soup = BeautifulSoup(my_content, "lxml")
 #print my_soup.prettify()
+my_soup = BeautifulSoup(my_content, "lxml")
 
-for my_item in my_soup:
-    tags_links = my_soup.find_all('a')
-    #print tags_links['href'], tags_links.contents[0]
-    print tags_links
+# find a tag
+a_tags = my_soup.findAll('a')
+for item in a_tags:
 
-    #wget.download(my_links, file_name)
+        # get href items
+        my_link = item['href']
+
+        if my_link.find('.pdf') != -1:
+            file_name = item.contents[0]
+
+            #replace spaces
+            file_no_spaces = file_name.replace(" ", "_")
+
+            # replace forward slash
+            file_replaced = file_no_spaces.replace("/", "-")+'.pdf'
+
+            # download pdf files
+            print ("downloading: ", my_link, file_replaced)
+            wget.download(my_link, './my-pubs/'+file_replaced)
+
+print('done')
+
 pass
 
